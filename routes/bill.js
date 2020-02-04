d6a4346ce7ea3ff0138272c439fa11972ac3c614
tashcain <shubhambaswal10@gmail.com>
2020-02-04 15:11:57 +0530

    le

--- routes/bill.js
+++ routes/bill.js
@@ -34,7 +34,7 @@ router.post("/savebill", (req, res) => {
   });
 });
 
-router.post("/getallbill", (req, res) => {
+router.get("/getallbill", (req, res) => {
   Bill.find({}, (err, bills) => {
     if (err) {
       res.send(err);
@@ -45,47 +45,37 @@ router.post("/getallbill", (req, res) => {
 });
 
 router.post("/getbillbydate", (req, res) => {
-  const date = req.body.customerName;
-  console.log(date);
-  Bill.find({ customerName: date }, (err, bills) => {
-    let bill = new Bill({
-      customerName: req.body.customerName,
-      title: req.body.title,
-      products: req.body.product,
-      totAmount: req.body.totAmt
-    });
-    bill.save(err => {
-      if (err) {
-        res.status(500).json({ success: false, msg: err });
-      } else {
-        res.json({ success: true, msg: bill });
-      }
-    });
+  var date = req.body.date;
+  Bill.find({ date: date }, (err, bills) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).json(bills);
+    }
   });
 });
 
 router.post("/billupdate", (req, res) => {
-  const customerName = req.body.customerName;
-  Bill.findOneAndUpdate({ customerName: customerName }, (err, bill) => {
-    if (err) {
-      res.send(err);
-    } else {
-      let bill = new Bill({
-        customerName: req.body.customerName,
-        invoiceNo: Math.floor(100000 + Math.random() * 900000),
-        title: req.body.title,
-        products: req.body.product,
-        totAmount: req.body.totAmt
-      });
-      bill.save(err => {
-        if (err) {
-          res.status(500).json({ success: false, msg: err });
-        } else {
-          res.json({ success: true, msg: bill });
-        }
-      });
+  var customerName = req.body.customerName;
+  Bill.findOneAndUpdate(
+    { customerName: customerName },
+    {
+      customerName: req.body.customerName,
+      title: req.body.title,
+      product: req.body.product,
+      totAmount: req.body.totAmt,
+      date: date + "-" + month + "-" + year,
+      paid: req.body.paid,
+      due: req.body.due
+    },
+    (err, bill) => {
+      if (err) {
+        res.statue(500).send(err);
+      } else {
+        res.status(200).send(bill);
+      }
     }
-  });
+  );
 });
 
 module.exports = router;
