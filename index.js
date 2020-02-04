50dfea17da2f5d4a1dbe646172714112fd01e2cc
tashcain <shubhambaswal10@gmail.com>
2020-02-04 15:16:10 +0530

    annu

--- index.js
+++ index.js
@@ -22,6 +22,9 @@ app.use("/auth", auth);
 const bill = require("./routes/bill");
 app.use("/bill", bill);
 
-const port = process.env.PORT || 3000;
+const client = require("./routes/client");
+app.use("/client", client);
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, console.log(`the server started at ${port}`));
